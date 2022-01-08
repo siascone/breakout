249210f94d106b40229c4ed4de7a3f4a659e3588
siascone <spencer.iascone@gmail.com>
2022-01-07 22:19:35 -0700

    bounce off the walls

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let x = canvas.width/2;
     let y = canvas.height-30;
-    const dx = 2;
-    const dy = -2
+    let dx = 2;
+    let dy = -2
+
+    const ballRadius = 10;
 
     function drawBall() {
         ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2);
+        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
         ctx.fillStyle = '#0095DD';
         ctx.fill();
         ctx.closePath();
@@ -18,8 +20,17 @@ document.addEventListener('DOMContentLoaded', () => {
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
+
         x += dx;
         y += dy;
+
+        if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+            dy = -dy;
+        }
+
+        if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+            dx = -dx;
+        }
     }
 
     setInterval(draw, 10);
