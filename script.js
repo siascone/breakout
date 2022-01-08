5da0ff2ef56a31390cd967dc9c7428693930838f
siascone <spencer.iascone@gmail.com>
2022-01-07 22:58:39 -0700

    Paddle and keyboard controls

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('myCanvas');
-    const ctx = canvas.getContext('2d');
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
 
-    let x = canvas.width/2;
-    let y = canvas.height-30;
-    let dx = 2;
-    let dy = -2
+    var ballRadius = 10;
+    var x = canvas.width/2;
+    var y = canvas.height-30;
+    var dx = 2;
+    var dy = -2
 
-    const ballRadius = 10;
+    var paddleHeight = 10;
+    var paddleWidth = 75;
+    var paddleX = (canvas.width-paddleWidth)/2;
+
+    var rightPressed = false;
+    var leftPressed = false;
+
+    document.addEventListener('keydown', keyDownHandler, false);
+    document.addEventListener('keyup', keyUpHandler, false);
+
+    function keyDownHandler(e) {
+        if (e.key == 'Right' || e.key == 'ArrowRight') {
+            rightPressed = true;
+        } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+            leftPressed = true;
+        }
+    }
+
+    function keyUpHandler(e) {
+        if (e.key == 'Right' || e.key == 'ArrowRight') {
+            rightPressed = false;
+        } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+            leftPressed = false;
+        }
+    }
 
     function drawBall() {
         ctx.beginPath();
-        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    function drawPaddle() {
+        ctx.beginPath();
+        ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
         ctx.fillStyle = '#0095DD';
         ctx.fill();
         ctx.closePath();
@@ -20,17 +53,30 @@ document.addEventListener('DOMContentLoaded', () => {
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
-
-        x += dx;
-        y += dy;
-
-        if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-            dy = -dy;
-        }
+        drawPaddle();
 
         if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
             dx = -dx;
         }
+        
+        if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+            dy = -dy;
+        }
+
+        if (rightPressed) {
+            paddleX += 5;
+            if (paddleX + paddleWidth > canvas.width) {
+                paddleX = canvas.width - paddleWidth;
+            }
+        } else if (leftPressed) {
+            paddleX -= 5;
+            if (paddleX < 0) {
+                paddleX = 0;
+            }
+        }
+
+        x += dx;
+        y += dy;
     }
 
     setInterval(draw, 10);
