2bd13a02bf43bb74758ced7362caf4e90ca9cdb4
siascone <spencer.iascone@gmail.com>
2022-01-07 23:04:49 -0700

    game over

--- script.js
+++ script.js
@@ -59,8 +59,16 @@ document.addEventListener('DOMContentLoaded', () => {
             dx = -dx;
         }
         
-        if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        if (y + dy < ballRadius) {
             dy = -dy;
+        } else if (y + dy > canvas.height - ballRadius) {
+            if (x > paddleX && x < paddleX + paddleWidth) {
+                dy = -dy;
+            } else {
+                alert('GAME OVER');
+                document.location.reload();
+                clearInterval(interval);
+            }            
         }
 
         if (rightPressed) {
@@ -79,6 +87,6 @@ document.addEventListener('DOMContentLoaded', () => {
         y += dy;
     }
 
-    setInterval(draw, 10);
+    var interval = setInterval(draw, 10);
 
 });
