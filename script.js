c7677e5d2bf6c798b2fe0a3a01f948b554155912
siascone <spencer.iascone@gmail.com>
2022-01-08 00:05:29 -0700

    mouse controles

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('keydown', keyDownHandler, false);
     document.addEventListener('keyup', keyUpHandler, false);
+    document.addEventListener('mousemove', mouseMoveHandler, false);
 
     function keyDownHandler(e) {
         if (e.key == 'Right' || e.key == 'ArrowRight') {
@@ -49,6 +50,13 @@ document.addEventListener('DOMContentLoaded', () => {
             rightPressed = false;
         } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
             leftPressed = false;
+        }
+    }
+
+    function mouseMoveHandler(e) {
+        let relativeX = e.clientX - canvas.offsetLeft;
+        if (relativeX > 0 && relativeX < canvas.width) {
+            paddleX = relativeX - paddleWidth/2
         }
     }
 
